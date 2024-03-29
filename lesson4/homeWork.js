3f891c01a5338a5d3d1375a95a8ba789646fa092
losev__152 <93211887+AlexanderLosev152@users.noreply.github.com>
2024-03-29 21:22:34 +0300

    Update homeWork.js

--- lesson4/homeWork.js
+++ lesson4/homeWork.js
@@ -1,101 +1,194 @@
 // 1 . Сделайте функцию, которая возвращает квадрат числа. Число передается параметром.
 
-function squareNumber(num) {
-  return num * num
-}
-console.log(squareNumber(6))
+// function squareNumber(num) {
+//   return num * num
+// }
+
+//--------------
+
+// const squareNumber = function (num) {
+//   return num * num
+// }
+
+//--------------
+
+// const squareNumber = (num) => {
+//   return num * num
+// }
+
+// console.log(squareNumber(5))
 
 // =================================================================
 
 // 2 . Сделайте функцию, которая возвращает сумму двух чисел.
 
-function sumNumbers(num) {
-  return num + num
-}
+// function sumNumbers(num) {
+//   return num + num
+// }
 
-console.log(sumNumbers(3))
+//--------------
+
+// const sumNumbers = function(num) {
+//   return num + num
+// }
+
+//--------------
+
+// const sumNumbers = (num) => {
+//   return num + num
+// }
+
+// console.log(sumNumbers(3))
 
 // =================================================================
 
 // 3 .Сделайте функцию, которая отнимает от первого числа второе и делит на третье.
 
-function takes(a, b, c) {
-  return (a - b) / c
-}
+// function takes(a, b, c) {
+//   return (a - b) / c
+// }
 
-console.log(takes(40, 10, 5))
+//--------------
+
+// const takes = function (a, b, c) {
+//   return (a - b) / c
+// }
+
+//--------------
+
+// const takes = (a, b, c) => {
+//   return (a - b) / c
+// }
+
+// console.log(takes(40, 10, 5))
 
 // =================================================================
 
 // 4 . Сделайте функцию, которая принимает параметром число от 1 до 7, а возвращает день недели на русском языке.
 
-function dayWeek(a) {
-  switch (a) {
-    case 1:
-      return 'Понедельник'
-      break
-    case 2:
-      return 'Вторник'
-      break
-    case 3:
-      return 'Среда'
-      break
-    case 4:
-      return 'Четверг'
-      break
-    case 5:
-      return 'Пятница'
-      break
-    case 6:
-      return 'Суббота'
-      break
-    case 7:
-      return 'Воскресенье'
-      break
-  }
-}
+// function dayWeek(a) {
+//   if (a === 1) return 'Понедельник'
+//   if (a === 2) return 'Вторник'
+//   if (a === 3) return 'Среда'
+//   if (a === 4) return 'Четверг'
+//   if (a === 5) return 'Пятница'
+//   if (a === 6) return 'Суббота'
+//   if (a === 7) return 'Воскресенье'
+// }
 
-console.log(dayWeek(5))
+//--------------
+
+// const dayWeek = function (a) {
+//   if (a === 1) return 'Понедельник'
+//   if (a === 2) return 'Вторник'
+//   if (a === 3) return 'Среда'
+//   if (a === 4) return 'Четверг'
+//   if (a === 5) return 'Пятница'
+//   if (a === 6) return 'Суббота'
+//   if (a === 7) return 'Воскресенье'
+// }
+
+//--------------
+
+// const dayWeek = (a) => {
+//   if (a === 1) return 'Понедельник'
+//   if (a === 2) return 'Вторник'
+//   if (a === 3) return 'Среда'
+//   if (a === 4) return 'Четверг'
+//   if (a === 5) return 'Пятница'
+//   if (a === 6) return 'Суббота'
+//   if (a === 7) return 'Воскресенье'
+// }
+
+// console.log(dayWeek(5))
 
 // =================================================================
 
 // 5 . Сделайте функцию, которая параметрами принимает 2 числа. Если эти числа равны - пусть функция вернет true, а если не равны - false.
 
-function bool(a, b) {
-  return a === b ? true : false
-}
+// function bool(a, b) {
+//   return a === b ? true : false
+// }
 
-console.log(bool(3, 3))
+//--------------
+
+// const bool = function (a, b) {
+//   return a === b ? true : false
+// }
+
+//--------------
+
+// const bool = (a, b) => {
+//   return a === b ? true : false
+// }
+
+// console.log(bool(3, 3))
 
 // =================================================================
 
 // 6 . Сделайте функцию, которая параметрами принимает 2 числа. Если их сумма больше 10 - пусть вернет true, а если нет то - false.
 
-function boolSum(a, b) {
-  return a + b > 10 ? true : false
-}
+// function boolSum(a, b) {
+//   return a + b > 10 ? true : false
+// }
 
-console.log(boolSum(6, 2))
+//--------------
+
+// const boolSum = function (a, b) {
+//   return a + b > 10 ? true : false
+// }
+
+//--------------
+
+// const boolSum = (a, b) => {
+//   return a + b > 10 ? true : false
+// }
+
+// console.log(boolSum(6, 2))
 
 // =================================================================
 
 // 7 . Сделайте функцию, которая параметром принимает число и проверяет - отрицательное оно или нет. Если отрицательное - пусть функция вернет true, а если нет - false.
 
-function negative(a) {
-  return a < 0 ? true : false
-}
+// function negative(a) {
+//   return a < 0 ? true : false
+// }
 
-console.log(negative(-6))
+//--------------
+
+// const negative = function (a) {
+//   return a < 0 ? true : false
+// }
+
+//--------------
+
+// const negative = (a) => {
+//   return a < 0 ? true : false
+// }
+
+// console.log(negative(-6))
 
 // =================================================================
 
 // 8 . Сделайте функцию isNumberInRange, которая параметром принимает число и проверяет, что оно больше нуля и меньше 10. Если это так - пусть функция возвращает true, если не так - false.
 
-function isNumberInRange(a) {
-  return a > 0 && a < 10 ? true : false
-}
+// function isNumberInRange(a) {
+//   return a > 0 && a < 10 ? true : false
+// }
 
-console.log(isNumberInRange(40))
+//--------------
+
+// const isNumberInRange = function (a) {
+//   return a > 0 && a < 10 ? true : false
+// }
+
+//--------------
+
+// const isNumberInRange = (a) => {
+//   return a > 0 && a < 10 ? true : false
+// }
+
+// console.log(isNumberInRange(40))
 
 // =================================================================
 
@@ -103,8 +196,21 @@ console.log(isNumberInRange(40))
 
 // =================================================================
 
-function isEven(a) {
-  return a % 2 === 0 ? true : false
-}
+// function isEven(a) {
+//   return a % 2 === 0 ? true : false
+// }
 
-console.log(isEven(2))
+//--------------
+
+// const isEven = function (a) {
+//   return a % 2 === 0 ? true : false
+// }
+
+//--------------
+
+// const isEven = (a) => {
+//   return a % 2 === 0 ? true : false
+// }
+
+// console.log(isEven(2))
+
