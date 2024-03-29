2553a883e1c99e0a2900c18920b8fea4e01d677c
losev__152 <93211887+AlexanderLosev152@users.noreply.github.com>
2024-03-29 19:51:58 +0300

    Add files via upload

---
+++ lesson4/main.js
@@ -0,0 +1,41 @@
+// function saiHi(name = "Guest") {
+//   console.log("Hi ${name}");
+// }
+
+// function sum(a, b = 1) {
+//   return a ** b;
+// }
+// console.log(sum(4));
+
+// function sumNum(value) {
+//   return value % 2 ? "не четное" : "четное";
+// }
+// console.log(sumNum(2));
+
+// const showSun = function (a, b) {
+//   console.log(a + b);
+// };
+
+// const copyShowSun = showSun;
+
+// showSun(21, 2);
+// copyShowSun(3, 4);
+
+// const sum2 = (a, b) => {
+//   console.log(a + b);
+//   return a + b;
+// };
+// sum2(4, 5);
+
+const doubleValue = (value) => value * 2;
+
+const str = (a, b) => {
+  for (let i = 0; i < b; i++) {
+    console.log(a);
+  }
+};
+
+str("hello", 5);
+
+// [а][у][о][и][э][ы];
+// Б, В, Г, Д, Ж, З, Й, К, Л, М, Н, П, Р, С, Т, Ф, Х, Ц, Ч, Ш, Щ
